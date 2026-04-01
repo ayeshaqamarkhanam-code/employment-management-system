b788b9e59718f4a42f093aaf47262ff798b5327c
Ayesha Qamar Khanam <ayeshaqamarkhanam@gmail.com>
2026-04-02 07:00:19 +0900

    feat: add DatabaseConnection class

---
+++ src/main/java/com/ems/dao/DatabaseConnection.java
@@ -0,0 +1,15 @@
+package com.ems.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/ems_db";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "mirzaISHRATHbaig@3205";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+}
